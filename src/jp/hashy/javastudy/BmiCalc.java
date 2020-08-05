6068d98688967bd4bb036d4c765c662de5f6194b
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-05 19:48:59 +0900

    bmi計算

---
+++ src/jp/hashy/javastudy/BmiCalc.java
@@ -0,0 +1,17 @@
+package jp.hashy.javastudy;
+
+public class BmiCalc {
+
+	public static void main(String[] args) {
+		double weight, height, bmi;
+		weight = 55;
+		height = 170;
+		height /= 100;
+		
+		bmi = weight / (height * height);
+		
+		System.out.println(bmi);
+
+	}
+
+}
