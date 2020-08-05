d8b5593ce8c0e2b4f8928d71653fe4e23f62edb2
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-05 21:11:06 +0900

    ゲッターメソッド

--- src/jp/hashy/javastudy/StaffInfo.java
+++ src/jp/hashy/javastudy/StaffInfo.java
@@ -15,6 +15,30 @@ class Staff {
 		this.stahhid = stahhid;
 		this.email = email;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStahhid() {
+		return stahhid;
+	}
+
+	public void setStahhid(int stahhid) {
+		this.stahhid = stahhid;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
 public class StaffInfo {
 	
@@ -22,7 +46,14 @@ public class StaffInfo {
 		Staff yamada = new Staff("Taro Yamada", 12345, "yamada@gmail");
 //		yamada.name = "Taro Yamada";
 		
-		yamada.sayhello();
+//		yamada.sayhello();
+		System.out.println("社員情報");
+		System.out.println("氏名：" + yamada.getName());
+		System.out.println("社員番号：" + yamada.getStahhid());
+		System.out.println("Email：" + yamada.getEmail());
+		
+		
+		
 	}
 
 }
