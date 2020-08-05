4ecc6086a4d32adb185c3734f82ea281d042ab8f
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-05 21:24:39 +0900

    クラスの継承

--- src/jp/hashy/javastudy/StaffInfo.java
+++ src/jp/hashy/javastudy/StaffInfo.java
@@ -40,10 +40,22 @@ class Staff {
 		this.email = email;
 	}
 }
+
+class RemoteStaff extends Staff {
+	public String location;
+	public RemoteStaff(String name, int stahhid, String email) {
+		super(name, stahhid, email);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+	
+}
+
 public class StaffInfo {
 	
 	public static void main(String[] args) {
 		Staff yamada = new Staff("Taro Yamada", 12345, "yamada@gmail");
+		RemoteStaff tanaka = new RemoteStaff("Hanako Tanaka", 23456, "tanaka@gmail");
+		tanaka.location = "大阪";
 //		yamada.name = "Taro Yamada";
 		
 //		yamada.sayhello();
@@ -52,7 +64,11 @@ public class StaffInfo {
 		System.out.println("社員番号：" + yamada.getStahhid());
 		System.out.println("Email：" + yamada.getEmail());
 		
-		
+		System.out.println("社員情報");
+		System.out.println("氏名：" + tanaka.getName());
+		System.out.println("社員番号：" + tanaka.getStahhid());
+		System.out.println("Email：" + tanaka.getEmail());
+		System.out.println("勤務地：" + tanaka.location);
 		
 	}
 
