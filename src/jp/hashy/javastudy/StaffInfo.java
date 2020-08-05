b54330c6796453dbcc15656b8095493219c8fc22
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-05 21:06:18 +0900

    コンストラクタ

--- src/jp/hashy/javastudy/StaffInfo.java
+++ src/jp/hashy/javastudy/StaffInfo.java
@@ -6,14 +6,21 @@ class Staff {
 	String email;
 	
 	public void sayhello() {
-		System.out.println("Hello" + this.name);
+		System.out.println("Hello " + this.name);
+	}
+
+	public Staff(String name, int stahhid, String email) {
+		super();
+		this.name = name;
+		this.stahhid = stahhid;
+		this.email = email;
 	}
 }
 public class StaffInfo {
 	
 	public static void main(String[] args) {
-		Staff yamada = new Staff();
-		yamada.name = "Taro Yamada";
+		Staff yamada = new Staff("Taro Yamada", 12345, "yamada@gmail");
+//		yamada.name = "Taro Yamada";
 		
 		yamada.sayhello();
 	}
